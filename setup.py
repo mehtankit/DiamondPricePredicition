75f80e774ce964860f028a7f08bef3a9d4c8efe3
mehtankit <ankit1.saramati@south.du.ac.in>
2024-08-07 21:44:05 +0530

    Project Setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = " -e ."
+
+def get_requiremetns(filepath:str)->List[str]:
+    requirements = []
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+        return requirements
+
+
+
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='Ankit',
+    author_email='ankit1.saramati@south.du.ac.in',
+    install_rqeuires=get_requiremetns("requirements.txt"),
+    packages=find_packages() # find_packges
+)
